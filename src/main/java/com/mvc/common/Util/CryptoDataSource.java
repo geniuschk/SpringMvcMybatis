9f13cfd101d06e2d78c14b59c3eaa4cda4207f44
geniuschk <geniuschk@dsmail.co.kr>
2015-07-02 18:53:41 +0900

    변수명 수정

--- src/main/java/com/mvc/common/Util/CryptoDataSource.java
+++ src/main/java/com/mvc/common/Util/CryptoDataSource.java
@@ -4,25 +4,25 @@ import org.apache.commons.dbcp.BasicDataSource;
 import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
 
 public class CryptoDataSource extends BasicDataSource {
-  StandardPBEStringEncryptor AES = new StandardPBEStringEncryptor();
+  StandardPBEStringEncryptor PBE = new StandardPBEStringEncryptor();
 
   @Override
   public synchronized void setUrl(String url) {
     // TODO Auto-generated method stub
-    AES.setPassword("geniuschk");
-    super.setUrl(AES.decrypt(url));
+    PBE.setPassword("geniuschk");
+    super.setUrl(PBE.decrypt(url));
   }
   
   @Override
   public void setPassword(String password) {
     // TODO Auto-generated method stub
-    super.setPassword(AES.decrypt(password));
+    super.setPassword(PBE.decrypt(password));
   }
 
   @Override
   public void setUsername(String username) {
     // TODO Auto-generated method stub
-    super.setUsername(AES.decrypt(username));
+    super.setUsername(PBE.decrypt(username));
   }
   
   
